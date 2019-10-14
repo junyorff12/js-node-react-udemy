73dd7b52c908def81205ba771e110d3a941d16a2
junyorff12 <junyorff12@gmail.com>
2019-10-14 18:31:29 -0300

    classes in JS

--- objeto/classe1.js
+++ objeto/classe1.js
@@ -9,17 +9,27 @@ class cicloFinanceiro {
   constructor(mes, ano) {
     this.mes = mes;
     this.ano = ano;
-    this.lancamentos = [];
+    this.meusLancamentos = [];
   }
 
   addLancamentos(...lancamentos) {
-    lancamentos.forEach(l => this.lancamentos.push);
+    lancamentos.forEach(lancamento => {
+      this.meusLancamentos.push(lancamento);
+    })
   }
 
   sumario() {
     let valorConsolidado = 0;
-    this.lancamentos.forEach(l => {
-      valorConsolidado += l.valor;
+    this.meusLancamentos.forEach(lancamento => {
+      valorConsolidado += lancamento.valor;
     })
+
+    return valorConsolidado
   }
 }
+
+const salario = new Lancamento('salario', 4500);
+const contaLuz = new Lancamento('luz', -220);
+const contas = new cicloFinanceiro(10, 2019);
+contas.addLancamentos(salario, contaLuz);
+console.log(contas.sumario());
