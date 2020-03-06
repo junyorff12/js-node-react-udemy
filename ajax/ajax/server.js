eb8af8b390b91bf928a1a55f606e71bf8a5884d1
junyorff12 <junyorff12@gmail.com>
2020-03-05 23:06:49 -0300

    XMLHttpRequest pt 2

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -6,6 +6,32 @@ app.use(express.static('.'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './upload');
+
+  },
+
+  filename: function(req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
+
+const upload = multer({ storage }).single('arquivo');
+
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      res.end('Deu bug...');
+    }
+
+    res.end('funfou...');
+  })
+})
+
+
 app.get('/teste', (req, res) => { res.send('tudo ok') });
 
 app.listen(8080, () => console.log('Executando...'));
